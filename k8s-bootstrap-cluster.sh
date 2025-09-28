eb0ac5e26117736d7eb9bce947510555a12472cf
Victor Andreasson <victor.andreasson@fripost.org>
2025-09-29 00:39:26 +0200

    script

--- k8s-bootstrap-cluster.sh
+++ k8s-bootstrap-cluster.sh
@@ -2,10 +2,9 @@
 
 # 0. Install Helm and add Argo CD repository
 # dnf install helm
-helm repo add argo-cd https://argoproj.github.io/argo-helm
  
-# 1. Install Argo CD using Helm chart
-helm install cluster-infra-apps/argo-cd
+# 1. Install Argo CD using Helm
+helm install cluster-infra-apps/argo-cd -n argo-cd --generate-name --create-namespace
 
 # 2. Deploy cluster infra apps
 kubectl apply -f cluster-infra-apps.yaml
